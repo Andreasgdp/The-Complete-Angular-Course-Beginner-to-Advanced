11102b2c752e9df30c1bb7b711ceef5d98a66763
Andreas <andreasgdp@gmail.com>
2020-01-19 18:52:27 +0100

    Stuff

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { AuthorsService } from "./authors.service";
+import { CoursesService } from "./courses.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoursesComponent } from "./courses.component";
+import { AuthorsComponent } from "./authors/authors.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, CoursesComponent, AuthorsComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
