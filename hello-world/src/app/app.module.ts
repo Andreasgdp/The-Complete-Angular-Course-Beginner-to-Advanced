bd715370a14888d3c4b5005cee0c4b04e3c3b4bd
Andreas <andreasgdp@gmail.com>
2020-01-21 22:03:36 +0100

    Working star that changes on click

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { AuthorsService } from "./authors.service";
-import { CoursesService } from "./courses.service";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { CoursesComponent } from "./courses.component";
-import { AuthorsComponent } from "./authors/authors.component";
+import { FavoriteComponent } from "./favorite/favorite.component";
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, AuthorsComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [CoursesService, AuthorsService],
+  declarations: [AppComponent, FavoriteComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
