1cc6273fda14ace95bdb7b4c949cf4dc37003a27
Andreas <andreasgdp@gmail.com>
2020-02-12 20:16:26 +0100

    Done for the day
    At lecture nr. 98

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,19 +1,30 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { FavoriteComponent } from "./favorite/favorite.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 import { MovieTitleComponent } from './movie-title/movie-title.component';
 import { MovieTitlePipe } from './movie-title.pipe';
 import { LikeComponent } from './like/like.component';
 import { ZippyComponent } from './zippy/zippy.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
-  declarations: [AppComponent, FavoriteComponent, MovieTitleComponent, MovieTitlePipe, LikeComponent, ZippyComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SignupFormComponent,
+        FavoriteComponent,
+        MovieTitleComponent,
+        MovieTitlePipe,
+        LikeComponent,
+        ZippyComponent,
+        NewCourseFormComponent
+    ],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
